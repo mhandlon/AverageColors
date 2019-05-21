492064b32786a1aaf733e8a4959054df84128b48
Michael Handlon <michael.handlon@gmail.com>
2019-05-21 18:20:37 -0400

    Initial Commit

---
+++ AvarageColors/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AvarageColors
+//
+//  Created by Michael Handlon on 5/21/19.
+//  Copyright © 2019 Michael Handlon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
